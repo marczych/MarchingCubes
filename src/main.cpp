fddcb9a72f276a1c1acdf5eaec612caea1656126
Marc Zych <marczych@gmail.com>
2013-02-23 15:04:47 -0800

    Glut window!

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,39 @@
+#ifdef __APPLE__
+#include "GLUT/glut.h"
+#include <OPENGL/gl.h>
+#endif
+
+#ifdef __unix__
+#include <GL/glut.h>
+#endif
+
+#ifdef _WIN32
+#include <GL/glew.h>
+#include <GL/glut.h>
+
+#pragma comment(lib, "glew32.lib")
+#pragma comment(lib, "OpenGL32.lib")
+#pragma comment(lib, "glu32.lib")
+#pragma comment(lib, "freeglut.lib")
+#endif
+
+void draw() {
+   // Draw here.
+}
+
+void glut(int argc, char** argv) {
+   glutInit(&argc, argv);
+   glutInitWindowPosition(100, 100);
+   glutInitWindowSize(100, 100);
+   glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);
+   glutCreateWindow("Geometry Creator Demo");
+   glutDisplayFunc(draw);
+
+   glutMainLoop();
+}
+
 int main(int argc, char** argv) {
+   glut(argc, argv);
+
    return 0;
 }
