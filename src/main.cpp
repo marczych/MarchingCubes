65c8d7a96692839a80f89453f925450055a7c380
Marc Zych <marczych@gmail.com>
2013-02-23 19:26:49 -0800

    Add OpenGL transformation helper

--- src/main.cpp
+++ src/main.cpp
@@ -17,12 +17,15 @@
 #pragma comment(lib, "freeglut.lib")
 #endif
 
+#include "MStackHelp.h"
 #include "MarchingCubes.h"
 #include <iostream>
 #include <glm/glm.hpp>
 
 using namespace glm;
 using namespace std;
+
+RenderingHelper ModelTrans;
 
 // Global variable for radius. Maybe we should find a better way to do this...
 // You know, without a global variable.
@@ -35,12 +38,26 @@ void draw() {
 void glut(int argc, char** argv) {
    glutInit(&argc, argv);
    glutInitWindowPosition(100, 100);
-   glutInitWindowSize(100, 100);
+   glutInitWindowSize(800, 600);
    glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);
    glutCreateWindow("Geometry Creator Demo");
    glutDisplayFunc(draw);
 
    glutMainLoop();
+}
+
+void openGLInitialize() {
+    glClearColor(0.8f, 0.8f, 1.0f, 1.0f);
+
+    glClearDepth(1.0f);
+    glDepthFunc(GL_LEQUAL);
+    glEnable(GL_DEPTH_TEST);
+
+    glEnable(GL_CULL_FACE);
+    glCullFace(GL_BACK);
+
+    ModelTrans.useModelViewMatrix();
+    ModelTrans.loadIdentity();
 }
 
 float implicitSphere(int x, int y, int z) {
