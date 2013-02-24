e35b44ed4aa7b6c03fbd24ebfe8e5dde6aa930c8
Christopher Patton <chpatton013@gmail.com>
2013-02-24 02:18:23 -0800

    rearrange edge masks

--- src/MarchingCubes.cpp
+++ src/MarchingCubes.cpp
@@ -13,14 +13,14 @@ void MarchingCubes::generateSurfaces() {
          for (int z = 0; z < size.z; ++z) {
             unsigned char edgeMask = 0;
 
-            edgeMask |= (points[index(x+1, y+1, z+1)] ? 0x01 : 0x00);
-            edgeMask |= (points[index(x+1, y+1, z  )] ? 0x02 : 0x00);
-            edgeMask |= (points[index(x+1, y,   z+1)] ? 0x04 : 0x00);
-            edgeMask |= (points[index(x+1, y,   z  )] ? 0x08 : 0x00);
-            edgeMask |= (points[index(x,   y+1, z+1)] ? 0x10 : 0x00);
-            edgeMask |= (points[index(x,   y+1, z  )] ? 0x20 : 0x00);
-            edgeMask |= (points[index(x,   y,   z+1)] ? 0x40 : 0x00);
             edgeMask |= (points[index(x,   y,   z  )] ? 0x80 : 0x00);
+            edgeMask |= (points[index(x+1, y,   z  )] ? 0x40 : 0x00);
+            edgeMask |= (points[index(x,   y+1, z  )] ? 0x08 : 0x00);
+            edgeMask |= (points[index(x+1, y+1, z  )] ? 0x04 : 0x00);
+            edgeMask |= (points[index(x,   y,   z+1)] ? 0x10 : 0x00);
+            edgeMask |= (points[index(x+1, y,   z+1)] ? 0x20 : 0x00);
+            edgeMask |= (points[index(x,   y+1, z+1)] ? 0x01 : 0x00);
+            edgeMask |= (points[index(x+1, y+1, z+1)] ? 0x02 : 0x00);
 
             cubes[index(x, y, z)] = edgeMask;
          }
